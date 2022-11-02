159b10fbe7a7ade92eb53a2378232f56b4d1c4c5
tashisrael <tashisrael04@gmail.com>
2022-11-02 19:11:13 +0300

    Add stylings and route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Quote from './components/Quote';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = '';
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
+const App = () => (
+  <Router>
+    <div className="App">
+      <Navbar />
+      <div className="components">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
       </div>
-    );
-  }
-}
+    </div>
+  </Router>
+
+);
+
+export default App;
